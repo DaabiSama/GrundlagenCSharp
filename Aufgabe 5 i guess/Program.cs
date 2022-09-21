7306eae3a5b50a431ae17a5126cf491df2e33ef7
Kaan Sahin <kaan.s16@gso.schule.koeln>
2022-09-21 14:42:14 +0200

    Aufgabe auseinander genommen

---
+++ Aufgabe 5 i guess/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Wie viel beträgt der Umsatz?");
+double Umsatz = Convert.ToDouble(Console.ReadLine());
+
+if (Umsatz <= 100 & Umsatz > 0 )
+{
+    Console.WriteLine("dem Kunden wird kein Rabatt gewährt");
+}
+else if(Umsatz > 500 )
+{
+    Console.WriteLine("Dem kunden wurde 10% Rabatt gewährt. Der Neue betrag ist {0:F2} ", (Umsatz * 0.90));
+}
+else if (Umsatz > 100 & Umsatz <= 500)
+{
+    Console.WriteLine("Dem Kunden wurde 5% Rabatt gewährt. Der Neue betrag ist {0:F2}", (Umsatz*0.95));
+}
+else
+{
+    Console.WriteLine("Etwas ist schief gelaufen");
+}
